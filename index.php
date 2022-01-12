b677744d52e8b744735b972cb11f535c8090dd9c
Dylan-Ay <dylan.ayache.68@gmail.com>
2022-01-12 17:04:02 +0100

    test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300&family=Open+Sans:ital,wght@0,300;0,400;0,500;1,300;1,400&display=swap" rel="stylesheet">	
 		<link rel="stylesheet" type="text/css" href="portfolio.css">
-		<title>Dylan Ayache - PortFolio</title>
+		<title>Dylan Ayache - PortFoli o</title>
 	</head>
 	<body>
 		<!------------ Include PHP ------------>
